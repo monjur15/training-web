a7230cabccb7634686c848cd59f5876331540177
monjur15 <monjur.git@gmail.com>
2021-12-17 20:13:43 +0600

    Navbar doing

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { } from './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#cources">Cources</Nav.Link>
+                            <Nav.Link href="#gallery">Gallery</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Navigation;
