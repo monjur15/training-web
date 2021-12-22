1b5c56b7a71d4136aabf6282e7c5ea1e7345f437
monjur15 <monjur.git@gmail.com>
2021-12-22 23:36:48 +0600

    Home Page started

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, ButtonGroup } from 'react-bootstrap';
 import Navigation from './components/Navigation/Navigation';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+
 
 
 function App() {
   return (
     <>
       <Navigation></Navigation>
+      <Home></Home>
       <Footer></Footer>
     </>
 
