f57ff63b146a39e81f276a3690c55611f99e0b73
monjur15 <monjur.git@gmail.com>
2021-12-25 21:48:58 +0600

    Who we are doing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { } from './Home.css';
 import slider1 from '../../images/1.svg';
+import weimg from '../../images/agency.jpg';
 import { Carousel, Form, Nav } from 'react-bootstrap';
 
 const Home = () => {
@@ -84,26 +85,29 @@ const Home = () => {
                 </Carousel.Item>
             </Carousel>
 
-            {/*  <div className='container slider_main'>
 
-                <div className='row slider1_div '>
-                    <div className='col-lg-6 col-sm-12 slider1_img p-4'>
-                        <img className='img-fluid' src={slider1} alt="" />
+
+            <div className='container'>
+                <div className='row we_div'>
+                    <div className="col-lg-7 we_content p-5">
+                        <h1>Who We Are</h1>
+                        <div className='down_border mb-lg-4'></div>
+                        <p>We are the fastest growing IT Training institute in Bangladesh.
+                            All our classes are currently online (Live Interactive) with students
+                            attending from all around the world. We are the first in Bangladesh
+                            to introduce online lab facility “KloudLab”, from basic concept
+                            building courses to latest bleeding edge courses are being covered by us.
+                            Our faculty members are well experienced in both training and production
+                            environments which makes them uniquely capable to conduct the training
+                            programs in a manner where candidates become prepared for the production
+                            environment.</p>
 
                     </div>
-                    <div className='col-lg-6 col-sm-12 slider1_content   mt-lg-4 p-4'>
-                        <h3>Are You Ready To</h3>
-                        <h1 className='mb-lg-5'>Lead The Way</h1>
-                        <div className='input_field w-75 mb-sm-4'><input className='w-100 p-lg-2 ' type="text"></input></div>
-                        <span className='search'><Nav.Link href="/search">{searchIcon}</Nav.Link></span>
-
+                    <div className="col-lg-5 we_img  mt-5">
+                        <img className='img-fluid w-75' src={weimg} alt="" />
                     </div>
-
                 </div>
 
-            </div> */}
-
-            <div className=''>
 
             </div>
 
