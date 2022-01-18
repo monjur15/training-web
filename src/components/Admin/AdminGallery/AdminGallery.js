a3720a4efba4420d4452ec4e9e9be10c170d4a47
TushitaTahsin <kazi26@student.sust.edu>
2022-01-18 11:23:17 +0600

    Updated admin gallery

--- src/components/Admin/AdminGallery/AdminGallery.js
+++ src/components/Admin/AdminGallery/AdminGallery.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import { Button, Dropdown, DropdownButton, FormControl, InputGroup } from 'react-bootstrap';
+import React, { useState } from "react";
+import {
+  Button,
+  Dropdown,
+  DropdownButton,
+  FormControl,
+  InputGroup,
+} from "react-bootstrap";
 
 const AdminGallery = () => {
-    function myFunction() {
-        alert("Done!");
-    }
-    return (
-        <div>
-            <div className="container py-5">
-                <h1 className='my-5'>Admin Gallery</h1>
+  function myFunction() {
+    alert("Done!");
+  }
 
+  const [photo, setPhoto] = useState(0);
 
-                <form>
-                    <label for="img">Select image:</label>
-                    <input type="file" id="img" name="img" accept="image/*"></input>
+  return (
+    <div>
+      <div className="container py-5">
+        <h1 className="my-5">Admin Gallery</h1>
 
-                    <div className='w-90'>
-                        <textarea class="contact-form-text" placeholder="Description"></textarea>
-                    </div>
-                    <div className='admin_btn'><Button onClick={() => myFunction()} variant="primary">Post</Button>{' '}</div>
+        <form>
+          <label for="img">Select image:</label>
+          <input type="file" id="img" name="img" accept="image/*"></input>
 
-
-
-                </form>
-
-
-            </div>
-        </div>
-    );
+          <div className="w-90">
+            <textarea
+              class="contact-form-text"
+              placeholder="Description"
+            ></textarea>
+          </div>
+          <div className="admin_btn">
+            <Button onClick={() => myFunction()} variant="primary">
+              Post
+            </Button>{" "}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AdminGallery;
