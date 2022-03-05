a9f47771cb9591cb8b523a5459b2ed8bd0f66661
TahsinTushita <tahsintushita@gmail.com>
2022-03-05 15:36:19 +0600

    Changed admin pages

--- src/components/Admin/AdminCourses/AdminCourses.js
+++ src/components/Admin/AdminCourses/AdminCourses.js
@@ -15,10 +15,23 @@ const AdminCourses = () => {
     console.log(courseIns);
   };
 
+  const [instructor, setInstructor] = useState();
+  const [category, setCategory] = useState();
+
   const [fee, setFee] = useState();
 
   const [classes, setClasses] = useState();
+  const [content, setContent] = useState();
+  const [credits, setCredits] = useState();
   const [image, setImage] = useState("");
+
+  const roles = [
+    { id: 1, role: "" },
+    { id: 2, role: "admin" },
+    { id: 3, role: "Rider" },
+    { id: 4, role: "Merchant" },
+  ];
+
   return (
     <div>
       <div className="container ad py-5">
@@ -44,21 +57,37 @@ const AdminCourses = () => {
           </div>
           {/* <div className='admin_btn'><Button variant="primary">Post</Button>{' '}</div> */}
 
-          <div className="" onChange={getCourseIns}>
-            <label htmlFor="DropdownButton">Instructor:</label>
+          {/* <div className="" onChange={getCourseIns}> */}
+          <label htmlFor="DropdownButton">Instructor:</label>
 
-            <DropdownButton
-              variant="outline-secondary"
-              title="Dropdown"
-              id="input-group-dropdown-1"
-            >
-              <Dropdown.Item href="#">Action</Dropdown.Item>
-              <Dropdown.Item href="#">Another action</Dropdown.Item>
-              <Dropdown.Item href="#">Something else here</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item href="#">Separated link</Dropdown.Item>
-            </DropdownButton>
-          </div>
+          {/* <DropdownButton
+            variant="outline-secondary"
+            title="Dropdown"
+            id="input-group-dropdown-1"
+            value={instructor}
+            onChange={(e) => setInstructor(e.target.value)}
+          >
+            <Dropdown.Item href="#">Action</Dropdown.Item>
+            <Dropdown.Item href="#">Another action</Dropdown.Item>
+            <Dropdown.Item href="#">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item href="#">Separated link</Dropdown.Item>
+          </DropdownButton> */}
+          {/* </div> */}
+
+          <select
+            className="w-100 ps-1 py-2 my-2 mt-2 form-bg"
+            label="role"
+            {...register("role")}
+            onChange={(e) => handleRole(e)}
+            value={instructor}
+          >
+            {roles.map((role) => (
+              <option key={role.id} value={role.role}>
+                {role.role}
+              </option>
+            ))}
+          </select>
 
           <br />
           <label htmlFor="fee">Course Fee:</label>
@@ -72,8 +101,8 @@ const AdminCourses = () => {
           ></input>
           <br />
           <br />
-          <label htmlFor="classes">Classes:</label>
 
+          <label htmlFor="classes">Classes:</label>
           <input
             type="number"
             id="classes"
@@ -83,6 +112,29 @@ const AdminCourses = () => {
           ></input>
           <br />
           <br />
+
+          <label htmlFor="content">Content:</label>
+          <input
+            type="number"
+            id="content"
+            name="content"
+            onChange={(e) => setContent(e.target.value)}
+            value={content}
+          ></input>
+          <br />
+          <br />
+
+          <label htmlFor="credits">Credits:</label>
+          <input
+            type="number"
+            id="credits"
+            name="credits"
+            onChange={(e) => setCredits(e.target.value)}
+            value={credits}
+          ></input>
+          <br />
+          <br />
+
           <label htmlFor="image">Select image:</label>
           <input type="file" onChange={(e) => setImage(e.target.files[0])} />
 
