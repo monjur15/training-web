2f618f5f332c7d9bce1280642471ca3ddd09b3bc
TahsinTushita <tahsintushita@gmail.com>
2022-04-13 13:39:36 +0600

    Fixed location error for now

--- src/components/CourseDetail/CourseDetail.js
+++ src/components/CourseDetail/CourseDetail.js
@@ -39,8 +39,10 @@ const CourseDetail = () => {
   };
 
   useEffect(() => {
+    console.log(location.state.courseId);
     dispatch(fetchSingleCourse(location.state.courseId));
     dispatch(fetchPopularCourses());
+    // window.location.reload();
   }, []);
 
   useEffect(() => {
@@ -67,7 +69,7 @@ const CourseDetail = () => {
           <div className="col-lg-6 col-md-12  course_det  row relative">
             <div className="course_card p-3 my-5 flex flex-col items-center">
               <div className="course_img my-3">
-                {singleCourse ? (
+                {singleCourse && singleCourse.length ? (
                   <img
                     src={url + "/media/" + singleCourse[0].course_img}
                     alt="img"
@@ -134,44 +136,44 @@ const CourseDetail = () => {
                 {/* {relatedCourses.map((course) => ( */}
                 {popularCourses && popularCourses.length
                   ? popularCourses.map(
-                      (course) =>
-                        course.course_title ===
-                        singleCourse[0].course_title ? null : (
-                          <div
-                            className="p-3 my-4 h-52 xl:h-44 bg-blue-300 rounded-2xl r_courses_card relative"
-                            key={course.course_id}
-                          >
-                            <div className="flex gap-5">
-                              {/* <div className="w-28 h-28 ml-5 rounded-full shadow-lg flex items-center justify-center"> */}
-                              <img
-                                className={classNames("ml-5", {
-                                  "w-24 h-16 sm:w-28 sm:h-20 my-auto":
-                                    course.course_title ===
-                                      "Learn AWS: Beginner to Solutions Architect - Associate" ||
-                                    course.course_title ===
-                                      "DevOps (Docker to Kubernetes)",
-                                  "w-20 h-20 sm:w-28 sm:h-28 my-auto":
-                                    course.course_title !==
-                                      "Learn AWS: Beginner to Solutions Architect - Associate" &&
-                                    course.course_title !==
-                                      "DevOps (Docker to Kubernetes)",
-                                })}
-                                src={url + "/media/" + course.course_img}
-                              ></img>
-                              {/* </div> */}
-                              <div>
-                                <h4 className="text-lg sm:text-xl md:text-lg xl:text-xl 2xl:text-2xl py-2 font-myriadProRegular font-bold text-uppercase text-white m-0">
-                                  {course.course_title}
-                                </h4>
-                                {screenSize.dynamicWidth > 640 ? (
-                                  <button className="bg-white text-blue-300 font-pangramRegular px-8 py-2 rounded-full shadow-md hover:shadow-lg transition-all duration-200">
-                                    Buy
-                                  </button>
-                                ) : null}
-                              </div>
+                      (course) => (
+                        // course.course_title ===
+                        // singleCourse[0].course_title ? null :
+                        <div
+                          className="p-3 my-4 h-52 xl:h-44 bg-blue-300 rounded-2xl r_courses_card relative"
+                          key={course.course_id}
+                        >
+                          <div className="flex gap-5">
+                            {/* <div className="w-28 h-28 ml-5 rounded-full shadow-lg flex items-center justify-center"> */}
+                            <img
+                              className={classNames("ml-5", {
+                                "w-24 h-16 sm:w-28 sm:h-20 my-auto":
+                                  course.course_title ===
+                                    "Learn AWS: Beginner to Solutions Architect - Associate" ||
+                                  course.course_title ===
+                                    "DevOps (Docker to Kubernetes)",
+                                "w-20 h-20 sm:w-28 sm:h-28 my-auto":
+                                  course.course_title !==
+                                    "Learn AWS: Beginner to Solutions Architect - Associate" &&
+                                  course.course_title !==
+                                    "DevOps (Docker to Kubernetes)",
+                              })}
+                              src={url + "/media/" + course.course_img}
+                            ></img>
+                            {/* </div> */}
+                            <div>
+                              <h4 className="text-lg sm:text-xl md:text-lg xl:text-xl 2xl:text-2xl py-2 font-myriadProRegular font-bold text-uppercase text-white m-0">
+                                {course.course_title}
+                              </h4>
+                              {screenSize.dynamicWidth > 640 ? (
+                                <button className="bg-white text-blue-300 font-pangramRegular px-8 py-2 rounded-full shadow-md hover:shadow-lg transition-all duration-200">
+                                  Buy
+                                </button>
+                              ) : null}
                             </div>
                           </div>
-                        )
+                        </div>
+                      )
                       // <div className="r_courses_card  my-4 p-3 row" key={course.id}>
                       //   <div className="col-4 ">
                       //     <img className="img-fluid" src={keyboard} alt="" />
--- src/components/Courses/PopularCourses.js
+++ src/components/Courses/PopularCourses.js
@@ -59,6 +59,7 @@ const PopularCourses = () => {
   }, [screenSize]);
 
   function navigateToCourseDetails(courseId) {
+    console.log(courseId);
     navigate("/coursesDetail", { state: { courseId: courseId } });
   }
 
@@ -98,7 +99,7 @@ const PopularCourses = () => {
           id="previousButton"
           onClick={() => swiperRef.current.swiper.slidePrev()}
         >
-          <button className="absolute bottom-60 left-14 w-10 h-10 rounded-full bg-white flex justify-center items-center drop-shadow-md hover:drop-shadow-xl text-SingleCourseOutlineBtn1 text-3xl transition-all duration-300 focus:outline-none">
+          <button className="absolute bottom-60 left-10 2xl:left-14 w-10 h-10 rounded-full bg-white flex justify-center items-center drop-shadow-md hover:drop-shadow-xl text-SingleCourseOutlineBtn1 text-3xl transition-all duration-300 focus:outline-none">
             <MdOutlineArrowBackIosNew className="text-PopularCourseCard hover:text-HomeBannerTop" />
           </button>
         </div>
@@ -109,7 +110,7 @@ const PopularCourses = () => {
           id="nextButton"
           onClick={() => swiperRef.current.swiper.slideNext()}
         >
-          <button class="absolute bottom-60 right-28 right-16 w-10 h-10 rounded-full bg-white flex justify-center items-center drop-shadow-md hover:drop-shadow-xl text-SingleCourseOutlineBtn1 text-3xl transition-all duration-300 focus:outline-none">
+          <button class="absolute bottom-60 right-28 right-10 2xl:right-16 w-10 h-10 rounded-full bg-white flex justify-center items-center drop-shadow-md hover:drop-shadow-xl text-SingleCourseOutlineBtn1 text-3xl transition-all duration-300 focus:outline-none">
             <MdOutlineArrowForwardIos className="text-PopularCourseCard hover:text-HomeBannerTop" />
           </button>
         </div>
